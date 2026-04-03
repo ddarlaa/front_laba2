ab31803408fcdef348a37fb0d379c432873c0c74
darla <d.pugina@list.ru>
2026-04-03 10:40:27 +0300

    changed stuff

--- public/js/main.js
+++ public/js/main.js
@@ -126,8 +126,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const faviconDarkExists = "false" === "true";
 
   if (faviconEl && faviconDarkExists) {
-    const lightFavicon = '/favicon.svg';
-    const darkFavicon = '/favicon-dark.svg';
+    const lightFavicon = '/front_laba2/favicon.svg';
+    const darkFavicon = '/front_laba2/favicon-dark.svg';
 
     const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
